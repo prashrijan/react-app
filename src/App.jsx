6f6d852c010ec1aad75d34d8502e215d0e5616ea
Prashrijan <shresthaprashrijan@gmail.com>
2024-11-26 09:54:19 +1100

    react app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <div className="bg-black h-screen ">
+        <h1 className="text-3xl text-white">Hello React World</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
